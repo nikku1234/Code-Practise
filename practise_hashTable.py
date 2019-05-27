c83785f22102309acf05a9e2ce24208c4ee11e5b
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-05-27 13:48:32 +0530

    Update practise_hashTable.py

--- practise_hashTable.py
+++ practise_hashTable.py
@@ -1,10 +1,12 @@
-nums[]=[1,2,3,4,5]
+nums=[11,2,13,4,6]
 target = 8
 hash_table={}
 for i in range(len(nums)):   
     hash_table[nums[i]]=i
+print(hash_table)
 for i in range(len(nums)):
     if target-nums[i] in hash_table:
+        print(hash_table[target-nums[i]])
         if hash_table[target-nums[i]] != i:
-                print (i, hash_table[target-nums[i]] )
+                print (i, hash_table[target-nums[i]])
 print("nothing")
