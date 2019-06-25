ecb4016214d118cd354a7abfc5f0a5fc5788cfab
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-06-25 15:20:23 +0530

    Create solution.py

---
+++ Nested Lists /solution.py
@@ -0,0 +1,17 @@
+def main():
+    N = int(input())
+    students = []
+    for i in range(N):
+        name = input()
+        mark = float(input())
+        students.append([name, mark])
+    
+    students.sort(key=lambda student: (student[1], student[0]))
+    #print(students)
+    second_lowest_mark = [student[1] for student in students if student[1] != students[0][1]][0]
+    #print([student[1] for student in students if student[1] != students[0][1]][0])
+    for s in [student for student in students if student[1] == second_lowest_mark]:
+       print(s[0])
+    
+if __name__ == '__main__':
+    main()
