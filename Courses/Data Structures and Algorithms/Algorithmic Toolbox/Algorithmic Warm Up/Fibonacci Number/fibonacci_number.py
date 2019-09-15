bdd8406763a1cf0bd3337ee2f03ede9f71cd7113
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-09-15 10:12:34 +0530

    files

--- Courses/Data Structures and Algorithms/Algorithmic Toolbox/Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
+++ Courses/Data Structures and Algorithms/Algorithmic Toolbox/Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
@@ -12,10 +12,15 @@ def fibonacci_number_naive(n):
 
 def fibonacci_number(n):
     assert 0 <= n <= 40
+    a,b = 0,1
+    for _ in range (n-1):
+        c= a+b
+        b,a=c,b
 
-    type here
+    return c
 
 
 if __name__ == '__main__':
     input_n = int(input())
     print(fibonacci_number(input_n))
+
