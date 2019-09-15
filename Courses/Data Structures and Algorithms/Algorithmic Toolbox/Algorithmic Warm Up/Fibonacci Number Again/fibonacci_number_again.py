8f31fdb1f41d5d63bcc9f0e1497c25f98406e475
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-09-15 14:14:49 +0530

    code

--- Courses/Data Structures and Algorithms/Algorithmic Toolbox/Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
+++ Courses/Data Structures and Algorithms/Algorithmic Toolbox/Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
@@ -18,15 +18,19 @@ def fibonacci_number_again(n, m):
     assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
 
     v1, v2, v3 = 1, 1, 0
+    #print(bin(n))
     # Perform fast exponentiation of the matrix (quickly raise it to the nth power)
     for rec in bin(n)[3:]:
         calc = (v2*v2) % m
+        print(rec,calc)
         v1, v2, v3 = (v1*v1+calc) % m, ((v1+v3)*v2) % m, (calc+v3*v3) % m
-        if rec == '1': v1, v2, v3 = (v1+v2) % m, v1, v2
-    print(v2);
+        if rec == '1':
+            v1, v2, v3 = (v1+v2) % m, v1, v2
+    return(v2)
 
 
 
 if __name__ == '__main__':
     input_n, input_m = map(int, input().split())
     print(fibonacci_number_again(input_n, input_m))
+
