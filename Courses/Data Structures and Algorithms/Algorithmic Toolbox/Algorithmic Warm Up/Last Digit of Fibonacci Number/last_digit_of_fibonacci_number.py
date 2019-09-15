258540138a07b43eda1670b98b0d8b18a4ae692e
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2019-09-15 15:03:37 +0530

    files

--- Courses/Data Structures and Algorithms/Algorithmic Toolbox/Algorithmic Warm Up/Last Digit of Fibonacci Number/last_digit_of_fibonacci_number.py
+++ Courses/Data Structures and Algorithms/Algorithmic Toolbox/Algorithmic Warm Up/Last Digit of Fibonacci Number/last_digit_of_fibonacci_number.py
@@ -12,14 +12,13 @@ def last_digit_of_fibonacci_number_naive(n):
 
 def last_digit_of_fibonacci_number(n):
     assert 0 <= n <= 10 ** 6
-
+    if n<=1:
+        return n
     a,b = 0,1
     for _ in range(n-1):
         c = a + b
-        a,b = c,b
+        b,a = c,b
     return c % 10
-
-
 
 if __name__ == '__main__':
     input_n = int(input())
