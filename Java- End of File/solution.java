9c62a0ade22851aad6d7c58a4f07222def9d6632
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-02-03 23:17:10 +0530

    Code

---
+++ Java- End of File/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your 
+    class should be named Solution. */
+    Scanner input = new Scanner(System.in);
+    int i =1;
+    while(input.hasNext())
+    {
+        System.out.print(i+" ");
+        System.out.println(input.nextLine());
+        i++;
+
+    }
+
+    }
+}
+
+
