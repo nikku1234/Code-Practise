e169a2c03a9e75cec1ca86717e0f930ae83dc02e
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-02-04 22:13:44 +0530

    code

---
+++ Conditional Statements/solution.c
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    cin>>a;
+    if(a==1)
+        cout<<"one";
+    else if(a==2)
+        cout<<"two";
+    else if(a==3)
+        cout<<"three";
+    else if(a==4)
+        cout<<"four";
+    else if(a==5)
+        cout<<"five";
+    else if(a==6)
+        cout<<"six";
+    else if(a==7)
+        cout<<"seven";
+    else if(a==8)
+        cout<<"eight";
+    else if(a==9)
+        cout<<"nine";
+    else
+        cout<<"Greater than 9";
+ 
+   return 0;
+}
+
