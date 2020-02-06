0685a5ca79f4cf9eaf68bb2a1cc9cfda17f2eb55
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-02-06 22:44:05 +0530

    code

---
+++ Java Date and Time/Solution.java
@@ -0,0 +1,31 @@
+
+import java.time.LocalDate;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        //return getDay(day, month, year);
+        int d = Integer.valueOf(day);
+        int m = Integer.valueOf(month);
+        int y = Integer.valueOf(year);
+
+        LocalDate date = LocalDate.of(y,m,d);
+        return date.getDayOfWeek().toString();
+
+
+    }
+
+}
+
+
+
