c0a85909b256b6340f2cb7bc405682b525ac0fd5
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-02-06 22:35:39 +0530

    added code

---
+++ Strings/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string s1,s2;
+
+    cin>>s1>>s2;
+
+    cout<<s1.size()<<" "<<s2.size();
+    cout<<"\n";
+    cout<<s1+s2;
+    cout<<"\n";
+    char c;
+    c = s1[0];
+    s1[0] = s2[0];
+    s2[0] = c;
+    cout<<s1<<" "<<s2;
+
+
+
+  
+    return 0;
+}
+
+
