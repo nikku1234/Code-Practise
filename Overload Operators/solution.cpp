9eb1cc9045202e35e57284617cc7549ab62b7097
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-02-07 22:30:17 +0530

    code

---
+++ Overload Operators/solution.cpp
@@ -0,0 +1,15 @@
+
+
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+
+ostream& operator<<(ostream& os, const Complex& c) {
+    return os << c.a << (c.b > 0 ? '+' : '-') << 'i' << c.b;
+}
+
+Complex operator+(const Complex& a, const Complex& b) { 
+    return { a.a + b.a, a.b + b.b };
+}
+
+
