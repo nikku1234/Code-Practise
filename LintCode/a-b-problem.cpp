234db86695537ca2e1fad45ebce6b037f3f7d3c3
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-05-23 22:06:41 +0530

    checking out lintcode

---
+++ LintCode/a-b-problem.cpp
@@ -0,0 +1,11 @@
+public class Solution {
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b
+     */
+    public int aplusb(int a, int b) {
+        // write your code here
+        return a+b;
+    }
+}
