71a12ff46c72da99cba296d376e772b06ce70156
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-04 22:22:47 +0530

    new solution

--- LeetCode/June/Reverse String.cpp
+++ LeetCode/June/Reverse String.cpp
@@ -14,3 +14,16 @@ public:
 
     }
 };
+
+//Another Solution
+class Solution {
+public:
+    string reverseString(string s) {
+        int i = 0, j = s.size() - 1;
+        while(i < j){
+            swap(s[i++], s[j--]);
+        }
+
+        return s;
+    }
+};
