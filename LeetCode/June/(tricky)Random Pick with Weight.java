bb99a20159f241539f6a804dbc7cb55b9ebf2cfc
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-05 20:54:48 +0530

    new

--- LeetCode/June/(tricky)Random Pick with Weight.java
+++ LeetCode/June/(tricky)Random Pick with Weight.java
@@ -23,3 +23,35 @@ class Solution {
         return left;
     }
 }
+
+// Without optimisation
+class Solution {
+    int cumilative_frequency[];
+    public Solution(int[] w) {
+         cumilative_frequency = new int[w.length];
+        cumilative_frequency[0] = w[0];
+        for(int i=1;i<w.length;i++){
+            cumilative_frequency[i] = cumilative_frequency[i-1] + w[i];
+        }
+
+
+    }
+
+    public int pickIndex() {
+        int length = cumilative_frequency.length;
+        int random = new Random().nextInt(cumilative_frequency[length-1])+1;
+        for(int i=0;i<length;i++){
+            if(cumilative_frequency[i]>random)
+                return i;
+        }
+        return 0;
+
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
+   
