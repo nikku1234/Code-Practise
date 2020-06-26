178b97edb931def2c78e99bfa2f6348831f5b068
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-26 21:11:53 +0530

    first code

--- Courses/Data Structures and Algorithms/Algorithmic Toolbox/Programming Challenges/Sum of Two Digits/sum_of_two_digits.py
+++ Courses/Data Structures and Algorithms/Algorithmic Toolbox/Programming Challenges/Sum of Two Digits/sum_of_two_digits.py
@@ -3,7 +3,7 @@
 
 def sum_of_two_digits(first_digit, second_digit):
     assert 0 <= first_digit <= 9 and 0 <= second_digit <= 9
-    return write your code here
+    return first_digit+second_digit
 
 
 if __name__ == '__main__':
