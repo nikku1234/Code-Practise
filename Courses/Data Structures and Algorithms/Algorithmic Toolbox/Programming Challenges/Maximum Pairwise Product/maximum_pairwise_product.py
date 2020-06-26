8da500057e834f7f34781ce6fd39f298df320f50
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-26 21:34:36 +0530

    maximum_pairwise product fast solution

--- Courses/Data Structures and Algorithms/Algorithmic Toolbox/Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
+++ Courses/Data Structures and Algorithms/Algorithmic Toolbox/Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
@@ -17,7 +17,18 @@ def max_pairwise_product_naive(numbers):
 def max_pairwise_product(numbers):
     assert len(numbers) >= 2
     assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
-    type here
+    max_index1 = -1
+    for i in range(n):
+        if max_index1 == -1 or numbers[i] > numbers[max_index1]:
+            max_index1 = i
+
+    max_index2 = -1
+    for i in range(n):
+        if i != max_index1 and (max_index2 == -1 or numbers[i] > numbers[max_index2]):
+            max_index2 = i
+
+    return numbers[max_index1] * numbers[max_index2]
+
 
 
 if __name__ == '__main__':
@@ -25,3 +36,6 @@ if __name__ == '__main__':
     input_numbers = list(map(int, input().split()))
     assert len(input_numbers) == n
     print(max_pairwise_product(input_numbers))
+    # n = 10
+    # A = [0] * n
+    # print(max_pairwise_product_naive(A))
