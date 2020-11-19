a63c39caef5c7566cf061625250260f22b9e58ba
nikku1234 <nikhilramesh1234@gmail.com>
2020-11-19 12:00:12 +0530

    hr

---
+++ HackerRank New/30 Days Code/Day 1: Data Types/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int temp1;
+        double temp2;
+        String temp3;
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        temp1 = scan.nextInt();
+        temp2 = scan.nextDouble();
+        scan.nextLine();
+        temp3 = scan.nextLine();
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+temp1);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d+temp2);
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+        System.out.println(s+temp3);
+        scan.close();
+    }
+}
---
+++ HackerRank New/30 Days Code/Day 1: Data Types/Solution.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+temp1 = 0
+temp2 = 0.0
+temp3 = ""
+# Read and save an integer, double, and String to your variables.
+temp1 = int(input())
+temp2 = float(input())
+temp3 = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+temp1)
+# Print the sum of the double variables on a new line.
+print(d+temp2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+temp3)
