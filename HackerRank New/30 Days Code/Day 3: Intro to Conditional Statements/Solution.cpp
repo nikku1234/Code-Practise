c46930855d42351f254d4e3bc1a63ccb3e290aee
nikku1234 <nikhilramesh1234@gmail.com>
2020-11-23 17:04:28 +0530

    Day 3

---
+++ HackerRank New/30 Days Code/Day 3: Intro to Conditional Statements/Solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int n = N;
+    if (n%2!=0){
+        cout<<"Weird";
+    }
+    else if (n>=2 and n<=5){
+        cout<<"Not Weird";
+    }
+
+    else if(n>=6 and n<=20){
+        cout<<"Weird";
+    }
+    else{
+        cout<<"Not Weird";
+    }
+
+
+    return 0;
+}
