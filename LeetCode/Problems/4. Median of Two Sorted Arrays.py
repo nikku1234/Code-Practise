9ec4a7ca47168054370cbeac8303640f140616cb
nikku1234 <nikhilramesh1234@gmail.com>
2020-12-08 13:22:10 +0530

    4

---
+++ LeetCode/Problems/4. Median of Two Sorted Arrays.py
@@ -0,0 +1,12 @@
+from numpy import median
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        num3 = nums1 + nums2
+        num3.sort()
+
+        return(median(num3))
