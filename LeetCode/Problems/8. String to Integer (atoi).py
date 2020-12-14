c3b37fcb9431ef8cdb83923c7482b3196d50af26
nikku1234 <nikhilramesh1234@gmail.com>
2020-12-14 13:17:10 +0530

    lc

---
+++ LeetCode/Problems/8. String to Integer (atoi).py
@@ -0,0 +1,97 @@
+# https://www.youtube.com/watch?v=zwZXiutgrUE
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type s: str
+        :rtype: int
+        """
+        """
+        :type str: str
+        :rtype: int
+        """
+        '''
+        Valid
+        '42' -> 42
+        '   -42' -> -42
+        '99999999999999' -> MAX_INT
+        '1312zcxvzc' -> 1312
+
+        Invalid
+        'asdfas'  -> 0
+        '  +asdkljfasd 9' -> 0
+        '' -> 0
+
+        1. whitespace
+        2. a +/- symbol
+        3. numbers
+        4  between MAX_INT and MIN_INT constraints
+        5. random characters
+
+        Time O(n)
+        Space O(1)
+
+        n is the number of characters in your input string
+        '''
+
+#         MAX_INT = 2 ** 31 - 1 # 2147483647
+#         MIN_INT = -2 ** 31    #-2147483648
+
+#         i = 0
+#         res = 0
+#         negative = 1
+
+
+#         #check for whitespace
+#         while i<len(str) and str[i] == ' ':
+#             i += 1
+
+#         # +/- Symbol
+
+#         if i<len(str) and str[i] == "-":
+#             i += 1
+#             negative = -1
+
+#         elif i<len(str) and str[i] == "+":
+#             i +=1
+
+#         #check for numbers
+#         number_checker = set('0123456789')
+
+#         while i < len(str) and str[i] in number_checker:
+#             res = res * 10 + int(str[i])
+#             i = i+1
+
+#         res = res * negative
+#         if res < 0:
+#             return max(res, MIN_INT)
+#         else:
+#             return min(res,MAX_INT)
+
+        MAX_INT = 2 ** 31 - 1 # 2147483647
+        MIN_INT = -2 ** 31    #-2147483648
+
+        i = 0
+        res = 0
+        negative = 1
+
+        #whitespace
+        while i < len(str) and str[i] == ' ':
+            i += 1
+
+        #+/- symbol
+        if i < len(str) and str[i] == '-':
+            i += 1
+            negative = -1
+        elif i < len(str) and str[i] == '+':
+            i += 1
+
+        #check number 0-9
+        checker = set('0123456789')
+        while i < len(str) and str[i] in checker:
+            if res > MAX_INT / 10 or (res == MAX_INT / 10 and int(str[i]) > 7):
+                return MAX_INT if negative == 1 else MIN_INT
+            res = res * 10  + int(str[i])
+            i += 1
+
+        #check the MAX / MIN int
+        return res * negative
