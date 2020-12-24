d52adc4d8b7ba68e589e839f56604db5b85bcc9f
nikku <nikhilramesh1234@gmail.com>
2020-12-25 02:12:45 +0530

    lc

--- LeetCode/Problems/11. Container With Most Water.java
+++ LeetCode/Problems/11. Container With Most Water.java
@@ -1,3 +1,4 @@
+// Brute Force
 class Solution {
     public int maxArea(int[] height) {
 
@@ -11,4 +12,35 @@ class Solution {
         return max_area;
 
     }
+}
+
+// Optimal
+class Solution {
+    public int maxArea(int[] height) {
+
+//         int max_area = 0;
+
+//         for(int i = 0; i < height.length; i++){
+//             for(int j = i+1; j < height.length; j++){
+//                 max_area = Math.max(max_area, Math.min(height[i], height[j]) * (j - i));
+//             }
+//         }
+//         return max_area;
+//     }
+
+
+        int max_area = 0;
+        int l = 0;
+        int r = height.length - 1;
+        while (l < r){
+            max_area = Math.max(max_area, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]){
+               l += 1;
+            }
+            else{
+                r -= 1;
+            }
+            }
+        return max_area;
+}
 }
