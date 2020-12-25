dd89fcb15cf8caa4bfc945e98498b661e0a9d7aa
nikku <nikhilramesh1234@gmail.com>
2020-12-26 00:09:59 +0530

    hr

---
+++ HackerRank New/Algorithms/Warmup/Birthday Cake Candles.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    dict = {}
+    for i in candles:
+        if i in dict.keys():
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    # print(dict)
+    return dict[max(dict.keys(), key=(lambda k: dict[k]))]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
