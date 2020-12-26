7c5502e5b66bd9ad140e3bc80c4917cfe63c3805
nikku <nikhilramesh1234@gmail.com>
2020-12-26 13:09:59 +0530

    hr

---
+++ HackerRank New/Algorithms/Implementation/Grading Students.py
@@ -0,0 +1,44 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    res = []
+    for i in grades:
+        if i >= 38:
+            if i % 5 == 3:
+                i += 2
+            elif i % 5 == 4:
+                i += 1
+        res.append(i)
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(raw_input().strip())
+
+    grades = []
+
+    for _ in xrange(grades_count):
+        grades_item = int(raw_input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
