d8f7bbbd7d035cfff074629dc3bb70296f652aa0
nikku <nikhilramesh1234@gmail.com>
2021-01-12 13:34:37 +0530

    Day 2

---
+++ 30-Days-Of-Python-master/day_2/variables.py
@@ -0,0 +1,16 @@
+# 'Day 2: 30 Days of python programming'
+
+first_name = "Nikhil"
+last_name = "Ramesh"
+full_name = first_name+' '+last_name
+country = "India"
+city = "Perinthalmanna"
+age = "23"
+year = "2021"
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+    'firstname' : 'Nikhil',
+    'lastname' : "Ramesh",
+    'fullname' : 'firstname'+' '+'lastname',
+    }
